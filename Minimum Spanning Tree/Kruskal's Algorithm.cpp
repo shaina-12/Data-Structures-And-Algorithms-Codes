158aac7c1a8f7e5dd00d9a6942c2aa5e7c7d5222
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2021-12-21 19:14:02 +0530

    Update Kruskal's Algorithm.cpp

--- Minimum Spanning Tree/Kruskal's Algorithm.cpp
+++ Minimum Spanning Tree/Kruskal's Algorithm.cpp
@@ -1,1 +1,86 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+//DSU data structure
+//path compression + rank by union
+class DSU{
+    int *parent;
+    int *rank;
+public:
+    DSU(int n){
+        parent = new int[n];
+        rank = new int [n];
+        for(int i=0;i<n;i++){
+            parent[i] = -1;
+            rank[i] = 1;
+        }
+    }
+    //find function
+    int find(int i){
+        if(parent[i] == -1){
+            return i;
+        }
+        return parent[i] = find(parent[i]);
+    }
+    //union function
+    int unite(int u, int v){
+        int s1 = find(u);
+        int s2 = find(v);
+        if(s1!=s2){
+            if(rank[s1] < rank[s2]){
+                parent[s1] = s2;
+                rank[s2] += rank[s1];
+            }
+            else{
+                parent[s2] = s1;
+                rank[s1] += rank[s2];
+            }
+        }
+    }
+};
+class Graph{
+    //represents edge list
+    vector<vector<int>> edgelist;
+    //no of vertices of a graph
+    int numV;
+public:
+    Graph(int numV){
+        this->numV = numV;
+    }
+    //add the edges and their weights in the edge list
+    void addEdge(int u, int v,int cost){
+        edgelist.push_back({u,v,cost});
+    }
+    //main function to find out the MST
+    int kruskalsMST(){
+        //1. sort all the edges
+        sort(edgelist.begin(),edgelist.end());
+        //initialize the DSU
+        DSU s(numV);
+        int ans = 0;
+        for(auto edge : edgelist){
+            int u = edge[0];
+            int v = edge[1];
+            int cost = edge[2];
+            //take the edge of MST if it does not form a cycle
+            if(s.find(u) != s.find(v)){
+                s.unite(u,v);
+                ans += cost;
+            }
+        }
+        return ans;
+    }
+};
+int main(){
+    Graph g(4);
+    g.addEdge(0,1,1);
+    g.addEdge(1,3,3);
+    g.addEdge(3,2,4);
+    g.addEdge(2,0,2);
+    g.addEdge(0,3,2);
+    g.addEdge(1,2,2);
+    int cost = g.kruskalsMST();
+    cout<<"The cost of MST is: "<<cost<<endl;
+}
 
