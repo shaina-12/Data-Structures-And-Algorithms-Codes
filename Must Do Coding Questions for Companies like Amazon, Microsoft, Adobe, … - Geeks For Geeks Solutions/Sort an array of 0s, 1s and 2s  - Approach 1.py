f37d1b93db2c44c98b1f0f56f654aae448588086
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2022-01-29 23:54:19 +0530

    Create Sort an array of 0s, 1s and 2s  - Approach 1.py

---
+++ Must Do Coding Questions for Companies like Amazon, Microsoft, Adobe, … - Geeks For Geeks Solutions/Sort an array of 0s, 1s and 2s  - Approach 1.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def sort012(self,arr,n):
+        # code here
+        arr.sort()
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.sort012(arr,n)
+        for i in arr:
+            print(i, end=' ')
+        print()
+
+# } Driver Code Ends
