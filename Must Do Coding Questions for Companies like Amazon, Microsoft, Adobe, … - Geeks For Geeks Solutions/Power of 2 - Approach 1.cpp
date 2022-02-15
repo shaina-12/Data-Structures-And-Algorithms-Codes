f4938cefc7454445223c442526159388af6f496e
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2022-02-15 22:32:04 +0530

    Create Power of 2 - Approach 1.cpp

---
+++ Must Do Coding Questions for Companies like Amazon, Microsoft, Adobe, … - Geeks For Geeks Solutions/Power of 2 - Approach 1.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+# include <cmath>
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here  
+        int itr = 0;
+        //long long num = 0
+        while(int(pow(2,itr))<n){
+            if(n%int(pow(2,itr))!=0){
+                return false;
+            }
+            itr++;
+        }
+        return true;
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
