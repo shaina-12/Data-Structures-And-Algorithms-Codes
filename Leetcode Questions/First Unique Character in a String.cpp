27d41509fcf82ead9020a8508ab1f7bda255f80e
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2022-07-24 19:23:05 +0530

    Create First Unique Character in a String.cpp

---
+++ Leetcode Questions/First Unique Character in a String.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map <char,int> m;
+        for(int i=0;i<s.length();i++){
+            m[s[i]]++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(m[s[i]]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
