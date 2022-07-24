bfd3a6360b484707552b429ad96c169786fe7a37
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2022-07-24 19:15:08 +0530

    Create Increasing Triplet Subsequence.cpp

---
+++ Leetcode Questions/Increasing Triplet Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int c1=INT_MAX, c2=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<=c1){
+                c1=nums[i];
+            }
+            else if(nums[i]<=c2){
+                c2=nums[i];
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
