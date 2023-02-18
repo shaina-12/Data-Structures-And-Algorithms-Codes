f2d6663055af07e50a6a48c423109fe4a6998fc1
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2023-02-18 22:59:35 +0530

    Update Rotate Array.cpp

--- Leetcode Questions/Rotate Array.cpp
+++ Leetcode Questions/Rotate Array.cpp
@@ -13,3 +13,19 @@ public:
         
     }
 };
+
+// Another Approach
+/*
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1||n==k){ // to deal with the case when (1) n=1 and n>=k (2) n=k
+            return;
+        }
+        reverse(nums.begin(),nums.begin()+n);
+        reverse(nums.begin(),nums.begin()+(k%n)); // k%n is done to deal with the condition when n>k. 
+        reverse(nums.begin()+(k%n),nums.end()); // Test case: Input: nums=[1,2] k=3 Correct Output: [2,1]
+    }
+};
+*/
