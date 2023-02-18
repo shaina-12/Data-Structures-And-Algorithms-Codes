be194f896998fa305d79bbf068d7baf2345780ae
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2023-02-18 23:00:27 +0530

    Update Contains Duplicate.cpp

--- Leetcode Questions/Contains Duplicate.cpp
+++ Leetcode Questions/Contains Duplicate.cpp
@@ -12,3 +12,22 @@ public:
         return false;
     }
 };
+
+// Another Approach
+/*
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,bool> hmap;
+        for(int i=0;i<nums.size();i++){
+            if(hmap.find(nums[i])==hmap.end()){
+                hmap[nums[i]]=i;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
+*/
