a9b3b131178efa644853314911332c43326d1573
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2023-04-22 01:56:21 +0530

    Update Radhesh and Hashing.cpp

--- IIIT Delhi M.Tech (CSE) Admission 2020 Mock Test Questions - CodeChef/Radhesh and Hashing.cpp
+++ IIIT Delhi M.Tech (CSE) Admission 2020 Mock Test Questions - CodeChef/Radhesh and Hashing.cpp
@@ -18,3 +18,34 @@ int main() {
 	}
 	return 0;
 }
+# cook your dish here
+import itertools as it
+
+
+def bounds(arr):
+    val = True
+    for i in range(len(arr)):
+        if(arr[i]<=0 or arr[i]>=25):
+            return False
+    return True
+
+t = int(input())
+while(t>0):
+    s = input()
+    ls = len(s)
+    hash_val = 0
+    for i in s:
+        hash_val+=ord(i)-97
+    li = list(it.permutations(range(hash_val),ls))
+    l1 = []
+    for i in range(len(li)):
+        if(sum(li[i])==hash_val and bounds(li[i])==True):
+            st = ""
+            for j in range(ls):
+                st += chr(li[i][j]+97)
+            if(st<=s):
+                l1.append(st)
+    l1.sort()
+    print(str(hash_val)+" "+l1[0])
+    t = t-1
+
