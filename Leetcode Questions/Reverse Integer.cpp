72945ae4d3cb76cc60456d5948e55a8f8bd6a9b3
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2023-04-26 00:59:22 +0530

    Update Reverse Integer.cpp

--- Leetcode Questions/Reverse Integer.cpp
+++ Leetcode Questions/Reverse Integer.cpp
@@ -15,3 +15,37 @@ public:
             return 0;
     }
 };
+
+// Another Apporach
+/*
+#include <cmath>
+class Solution {
+public:
+    int reverse(int x) {
+        int sign = 0;
+        long long int max = pow(2,31)-1, min = -1*pow(2,31);
+        long long int val;
+        // 0 is for positive and 1 is for negative (Following 2's Complement Form)
+        if(x<0){
+            sign = 1;
+            val  = abs(x);
+        }
+        else{
+            val = x;
+        }
+        //cout<<val<<" "<<sign<<endl;
+        long long int r = 0;
+        while(val>0){
+            r = (r*10) + (val%10);
+            val = val/10;
+        }
+        if(r>max || r<min){
+            return 0;
+        }
+        if(sign==1){
+            r = -1*r;
+        }
+        return r;
+    }
+};
+*/
