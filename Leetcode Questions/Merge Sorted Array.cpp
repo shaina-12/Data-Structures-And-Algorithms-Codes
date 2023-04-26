1c34f0d55431c8d102d118f19d15fe0a8b452a62
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2023-04-26 17:04:08 +0530

    Update Merge Sorted Array.cpp

--- Leetcode Questions/Merge Sorted Array.cpp
+++ Leetcode Questions/Merge Sorted Array.cpp
@@ -31,3 +31,48 @@ public:
         }
     }
 };
+
+
+/*
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> v;
+        if(n==0){
+            return;
+        }
+        if(m==0){
+            for(int i=0;i<n;i++){
+                nums1[i]=nums2[i];
+            }
+            return;
+        }
+        int j=0,k=0;
+        while(j<m && k<n){
+            if(nums1[j]<nums2[k]){
+                v.push_back(nums1[j]);
+                j++;
+            }
+            else{
+                v.push_back(nums2[k]);
+                k++;
+            }
+        }
+        if(j==m){
+            while(k<n){
+                v.push_back(nums2[k]);
+                k++;
+            }
+        }
+        else{
+            while(j<m){
+                v.push_back(nums1[j]);
+                j++;
+            }
+        }
+        for(int i=0;i<m+n;i++){
+            nums1[i]=v[i];
+        }
+    }
+};
+*/
