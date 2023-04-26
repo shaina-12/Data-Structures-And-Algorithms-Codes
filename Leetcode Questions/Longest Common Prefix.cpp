96e4081c31a5869325ce802ef2d301cb3e4c5565
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2023-04-26 16:40:41 +0530

    Update Longest Common Prefix.cpp

--- Leetcode Questions/Longest Common Prefix.cpp
+++ Leetcode Questions/Longest Common Prefix.cpp
@@ -21,3 +21,37 @@ public:
         return prefix;
     }
 };
+
+/*
+class Solution {
+public:
+    
+    string prefix(string s1, string s2){
+        string str = "";
+        int n;
+        if(s1.length()<s2.length()){
+            n=s1.length();
+        }
+        else{
+            n=s2.length();
+        }
+        for(int i=0;i<n;i++){
+            if(s1[i]!=s2[i]){
+                break;
+            }
+            str+=s1[i];
+        }
+        return str;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        string refstr = strs[0];
+        for(int i=1;i<strs.size();i++){
+            string newstr = prefix(refstr,strs[i]);
+            if(newstr.length()<refstr.length()){
+                refstr=newstr;
+            }
+        }
+        return refstr;
+    }
+};
+*/
